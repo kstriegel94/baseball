79dae05c0507b99ea55403244287229e8c290e70
Kimberly Striegel <kimberlystriegel@gmail.com>
2016-10-25 12:16:23 -0700

    update

--- index.js
+++ index.js
@@ -4,6 +4,6 @@ var path = require('path');
 
 app.use(express.static(path.join(__dirname, 'public')));
 
-var server = app.listen(process.env.PORT || 3000, function(){
+var server = app.listen(process.env.PORT || 3500, function(){
 	console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
 });
--- public/scenes/start.js
+++ public/scenes/start.js
@@ -11,7 +11,34 @@ Crafty.defineScene('Start', function(attrs) {
       weight: 'bold'
     });
 
+  var direction = Crafty.e('Canvas, Text')
+    .text('Use directional keys to move crosshair')
+    .textFont({
+      family: 'Arial',
+      size: '20px',
+      weight: 'bold'
+    });
+
+  var direction2 = Crafty.e('Canvas, Text')
+    .text('Use spacebar to hit ball')
+    .textFont({
+      family: 'Arial',
+      size: '20px',
+      weight: 'bold'
+    });
+
+  var direction3 = Crafty.e('Canvas, Text')
+    .text('Three strikes and you lose')
+    .textFont({
+      family: 'Arial',
+      size: '20px',
+      weight: 'bold'
+    });
+
   title.attr({x: (Crafty.viewport._width - title.w) / 2});
+  direction.attr({x: (Crafty.viewport._width - direction.w) / 2, y: 150});
+  direction2.attr({x: (Crafty.viewport._width - direction2.w) / 2, y: 200});
+  direction3.attr({x: (Crafty.viewport._width - direction3.w) / 2, y: 250});
 
   var start_button = Crafty.e('Button, Text, Canvas')
     .attr({x: 0, y: 100})
